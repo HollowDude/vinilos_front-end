04298f4ca2a9815e744b2159fc88dcb74ae646f1
HollowDude <madafakn@gmail.com>
2025-05-20 09:40:11 -0700

    To checkout

--- src/types/commons.ts
+++ src/types/commons.ts
@@ -1,1 +1,3 @@
 export const BACKEND = process.env.NEXT_PUBLIC_API_URL || "https://vinilos-backend-2tpu.onrender.com"
+
+//export const BACKEND = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000"
